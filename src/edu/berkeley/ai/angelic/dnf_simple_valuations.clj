9fb731d1e3d3c0545b4bdbd529da01e24691fb58
Jason Wolfe <jason@w01fe.com>
2009-03-24 17:10:41 -0700

    Add actual subsumption logic to dnf simple valuations

--- src/edu/berkeley/ai/angelic/dnf_simple_valuations.clj
+++ src/edu/berkeley/ai/angelic/dnf_simple_valuations.clj
@@ -71,17 +71,34 @@
 
 
 ;; For now, a valid subsumption map maps predicates to clojure predicates.
-; One valuation subsumes another if every possibly-true value in the latter is subsumed by a possibly-true value in former.
-
+; One valuation subsumes another if, for each clause, the best possibly-true value in the latter is subsumed by the best possibly-true value in the former.
+; Right now, is limited to single clauses and single-argument predicates.
 
 (defmethod get-valuation-states     ::DNFSimpleValuation [val subsumption-map]
-  (util/assert-is (empty? subsumption-map))
-  [(:dnf val) *no-subsumption-info*])
+  (if (empty? subsumption-map)
+      [(:dnf val) *no-subsumption-info*]
+    (let [dnf (:dnf val)]
+      (util/assert-is (= (count dnf) 1))
+      (loop [pairs (seq (first dnf)), states (first dnf), sub-info (util/map-vals (constantly nil) subsumption-map)]
+	(if (nil? pairs) [states {:class ::DNFSimpleValuationSI :sub-info sub-info}]
+	  (let [[atom tv] (first pairs)]
+	    (if (contains? sub-info (first atom))
+	        (recur (next pairs) 
+		       (dissoc states atom) 
+		       (assoc sub-info (first atom)
+			      (if-let [old (get sub-info (first atom))]
+				  (if ((get subsumption-map (first atom)) old (second atom)) old (second atom))
+				(second atom))))
+	      (recur (next pairs) states sub-info))))))))
+      
 
 
 (defmethod valuation-subsumes?     [::DNFSimpleValuationSI ::DNFSimpleValuationSI] [val1 val2 subsumption-map]
-  (throw (IllegalArgumentException.))
-  )
+  (let [si1 (util/safe-get val1 :sub-info)
+	si2 (util/safe-get val2 :sub-info)]
+    (every? 
+     (fn [pred] ((get subsumption-map pred) (get si1 pred) (get si2 pred)))
+     (keys subsumption-map))))
 
 
 (defmethod extract-a-state          ::DNFSimpleValuation [v]
@@ -130,3 +147,29 @@
   (let [c (first (filter identity (for [c1 (:dnf v1) c2 (:dnf v2)] (intersect-dnf-clauses c1 c2))))]
     (util/assert-is (identity c))
     (make-dnf-simple-valuation #{c} (:bound v1))))
+
+
+
+(util/deftest dnf-subsumption
+  (let [sub-info {'a > 'b <}]
+    (util/is (valuation-subsumes? 
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 4] :true [b 1] :unknown}} 5) sub-info))
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 3] :unknown [b 3] :true}} 2) sub-info))
+	      sub-info))
+    (util/is (valuation-subsumes? 
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 4] :true [b 1] :unknown}} 2) sub-info))
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 3] :unknown [b 3] :true}} 5) sub-info))
+	      sub-info))
+    (util/is (not (valuation-subsumes? 
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 4] :true [b 5] :unknown}} 5) sub-info))
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 3] :unknown [b 3] :true}} 2) sub-info))
+	      sub-info)))
+    (util/is (not (valuation-subsumes? 
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 4] :true [b 1] :unknown}} 5) sub-info))
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[a 5] :unknown [b 3] :true}} 2) sub-info))
+	      sub-info)))
+    (util/is (valuation-subsumes? 
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[c] :true [a 4] :true [b 1] :unknown}} 5) sub-info))
+	      (second (get-valuation-states (make-dnf-simple-valuation '#{{[c] :true [a 3] :unknown [b 3] :true}} 2) sub-info))
+	      sub-info))
+    ))
