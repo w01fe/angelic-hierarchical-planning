5a56141a12692b462383d3986d40ee8a8db8dd5a
Jason Wolfe <jawolfe@berkeley.edu>
2009-07-31 11:14:51 -0700

    Interim commit, restructuring lib system.

--- src/edu/berkeley/ai/angelic/load_all.clj
+++ src/edu/berkeley/ai/angelic/load_all.clj
@@ -1,7 +1,10 @@
 (ns edu.berkeley.ai.angelic
  (:refer-clojure)
  (:require  [edu.berkeley.ai [util :as util] [envs :as envs]]
-            [edu.berkeley.ai.domains.strips :as strips]
+            [edu.berkeley.ai.domains.strips :as strips])
+ (:load "edu/berkeley/ai/angelic/angelic.clj"
+	"edu/berkeley/ai/angelic/descriptions_and_valuations.clj")
+ (:require
             [edu.berkeley.ai.angelic angelic
 	     descriptions-and-valuations ncstrips-descriptions dnf-valuations 
 ;	     hybrid-dnf-simple-valuations hybrid-ncstrips-descriptions 
