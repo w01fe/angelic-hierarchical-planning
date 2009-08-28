3191c524e2c911d3cd7b14836f28dedd42508ad0
Jason Wolfe <jawolfe@berkeley.edu>
2009-08-28 03:06:52 -0700

    a whole new world

--- ros/angelic_planning/src/ros/world.clj
+++ ros/angelic_planning/src/ros/world.clj
@@ -404,12 +404,16 @@
 	     :height 0.6}
 
     "bottle" {:xyz [16.3 26.1 0.85] :rpy [0 0 0] :def (get-odwalla)
-	     :type :movable :on "table" :goal nil ;["table" (make-xy-region [17.7 18.0] [26.0 26.2])] 
+	     :type :movable :on "table" :goal ["table" (make-xy-region [17.7 18.0] [26.0 26.2])] 
 	      :height 0.2}
     "bottle2" {:xyz [16.3 27.0 0.85] :rpy [0 0 0] :def (get-odwalla)
 	     :type :movable :on "table" :goal ["endtable"] #_["table" (make-xy-region [16.21 16.8] [26.2 26.5])] :height 0.2}
     "bottle3" {:xyz [12.4 26.1 0.70] :rpy [0 0 0] :def (get-odwalla)
 	     :type :movable :on "endtable" :goal ["table" ] :height 0.2}
+
+    "bottle4" {:xyz [18.8 26.5 0.85] :rpy [0 0 0] :def (get-odwalla)
+	     :type :movable :on "table" :goal ["table" (make-xy-region [16.21 17.0] [26.1 27.0])] 
+	      :height 0.2}
     }
       d3-res d2-res d2-pad))
 
