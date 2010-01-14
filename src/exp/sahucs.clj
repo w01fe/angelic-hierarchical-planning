34805f6952cd4eb6ea6b0e2f49baed122aa5e220
Jason Wolfe <jason@w01fe.com>
2010-01-13 17:00:17 -0800

    New code cleanup

--- src/exp/sahucs.clj
+++ src/exp/sahucs.clj
@@ -12,32 +12,33 @@
 ;; This version may be inefficient in graphy domains, but should still be complete+optimal
 ;; (as long as rewards for primitives are strictly negative)
 
-;; TODO: pass incremental updates upwards. 
-;; Difficulty: nodes with multiple ancestors.
-
-
-
-(deftype PartialResult [result-map cutoff])
-
 
 ;; A PartialResult stores a map from states to rewards, where a state is present
 ;; iff it has reward > cutoff. 
+(deftype PartialResult [result-map cutoff])
+
+
 
 (deftype SANode [context action result-map-atom queue])
 
-;(defstruct sanode-struct :context :action :result-map-atom :queue)
-;(defn SANode [c a r q] (struct sanode-struct c a r q))
+(defn cutoff [node]
+  (- (nth (queues/g-pq-peek-min (:queue node)) 1)))
 
-;; Represents an action sequence from a state, with sanode representing the first action in remaining-actions.
- ; (or nil, if remaining-actions is empty.)
+
+;; Represents an action sequence from a state, with sanode representing the first action
+; in remaining-actions. (or nil, if remaining-actions is empty.)
 (deftype SANodeEntry [state sanode reward-to-state remaining-actions hash-code] :as this
   Object
-  (equals [y] (or (identical? this y) (and (= state (:state y)) (= remaining-actions (:remaining-actions y)))))
+  (equals [y] (or (identical? this y) 
+                  (and (= state (:state y)) (= remaining-actions (:remaining-actions y)))))
   (hashCode [] hash-code))
 
 (defn make-sanode-entry [state sanode reward-to-state remaining-actions]
   (SANodeEntry state sanode reward-to-state remaining-actions
-               (unchecked-add (int (hash state)) (unchecked-multiply (int 13) (int (hash remaining-actions))))))
+               (unchecked-add (int (hash state)) 
+                              (unchecked-multiply (int 13) (int (hash remaining-actions))))))
+
+
 
 (defn make-queue [initial-elements]
   (let [q (queues/make-graph-search-pq)]
@@ -50,31 +51,38 @@
     (do (assert (pred (get m k) v)) m)
     (assoc m k v)))
 
-; empty refinement !
 
-(defn- get-sa-node [#^HashMap cache s a]
+(defn extract-effect [state context opt]
+  (vary-meta (env/extract-effects state context) assoc :opt opt))
+
+(defn stitch-effect-map [effect-map state reward-to-state]
+  (util/map-map1 
+   (fn [[effects local-reward]]
+     [(vary-meta (env/apply-effects state effects) assoc 
+                 :opt (concat (:opt (meta state)) (:opt (meta effects))))
+      (+ reward-to-state local-reward)]) 
+   effect-map))
+
+
+
+(declare get-sa-node)
+(defn get-sanode-entry [cache state reward-to-state actions]
+  (make-sanode-entry state 
+   (when (seq actions) (get-sa-node cache state (first actions)))
+   reward-to-state actions))
+
+(defn get-sa-node [#^HashMap cache s a]
   "Create a new sa-node, or returned the cached copy if it exists."
-;  (println "GET SA" (env/action-name a))
   (let [context (env/precondition-context a s)]
     (util/cache-with cache [(env/action-name a) (env/extract-context s context)]
       (let [s     (env/get-logger s)
             prim? (env/primitive? a)
             [ss r] (when-let [x (and prim? (env/applicable? a s) (env/successor a s))] x)] ;pun        
         (SANode context a 
-          (atom (if ss {(vary-meta (env/extract-effects ss context) assoc :opt [a]) r} {})) 
+          (atom (if ss {(extract-effect ss context [a]) r} {})) 
           (make-queue (for [ref (when-not prim? (hierarchy/immediate-refinements a s))]
-                        [(make-sanode-entry s (when (seq ref) (get-sa-node cache s (first ref))) 0.0 ref) 0.0])))))))
+                        [(get-sanode-entry cache s 0.0 ref) 0.0])))))))
 
-
-(defn cutoff [node]
-  (- (nth (queues/g-pq-peek-min (:queue node)) 1)))
-
-(defn stitch-results [effect-map state reward-to-state]
-  (util/map-map1 
-   (fn [[effects local-reward]]
-     [(vary-meta (env/apply-effects state effects) assoc :opt (concat (:opt (meta state)) (:opt (meta effects))))
-      (+ reward-to-state local-reward)]) 
-   effect-map))
 
 ;; May return states better than next-best, but these will be held at the parent.
 (defn expand-sa-node [node #^HashMap cache next-best state reward-to-state last-cutoff]
@@ -83,19 +91,18 @@
      (if (< (cutoff node) next-best)
          (PartialResult (stitch-results new-results state reward-to-state) (cutoff node))   
        (let [[entry neg-reward] (queues/g-pq-remove-min-with-cost! (:queue node))
-             b-s (:state entry), b-rts (:reward-to-state entry), b-ra (:remaining-actions entry), b-sa (:sanode entry)
-             rec-next-best (max next-best (cutoff node))]
+             b-s (:state entry), b-rts (:reward-to-state entry), 
+             b-ra (:remaining-actions entry), b-sa (:sanode entry)
+             rec-next-best (- (max next-best (cutoff node)) b-rts)]
            (if (empty? b-ra)
-               (let [eff (vary-meta (env/extract-effects b-s (:context node)) assoc :opt (:opt (meta b-s)))]
+               (let [eff (extract-effect b-s (:context node) (:opt (meta b-s)))]
                  (swap! (:result-map-atom node) assoc-safe >= eff b-rts)
                  (recur (assoc-safe new-results >= eff b-rts)))
-             (let [rec (expand-sa-node b-sa cache (- rec-next-best b-rts) b-s b-rts (- 0 neg-reward b-rts))]
+             (let [rec (expand-sa-node b-sa cache rec-next-best b-s b-rts (- 0 neg-reward b-rts))]
                (when (> (:cutoff rec) Double/NEGATIVE_INFINITY) 
                  (queues/g-pq-replace! (:queue node) entry (- 0 b-rts (:cutoff rec))))
                (doseq [[ss sr] (:result-map rec)]
-                 (queues/g-pq-add! (:queue node)
-                   (make-sanode-entry ss (when (next b-ra) (get-sa-node cache ss (second b-ra))) sr (next b-ra))
-                   (- sr)))
+                 (queues/g-pq-add! (:queue node) (get-sanode-entry cache ss sr (next b-ra)) (- sr)))
                (recur new-results)))))))
 
 
@@ -107,10 +114,8 @@
         root  (get-sa-node cache init (hierarchy/TopLevelAction e [(hierarchy/initial-plan henv)]))]
     (loop [cutoff 0 last-cutoff 0]
       (let [result (expand-sa-node root cache cutoff init 0.0 last-cutoff)]
-;        (println "TOP" (:cutoff result) cutoff last-cutoff result)
         (cond (not (empty? (:result-map result)))
                 (let [[k v] (util/first-maximal-element val (:result-map result))]
-;                  (println k)
                   [(:opt (meta k)) v])
               (> (:cutoff result) Double/NEGATIVE_INFINITY)
                 (recur (:cutoff result) cutoff))))))
