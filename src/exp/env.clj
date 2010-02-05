1db1b99e0efb104c237613fdea78e71632dcf6ce
Jason Wolfe <jason@w01fe.com>
2010-02-04 21:24:34 -0800

    Add tests.

--- src/exp/env.clj
+++ src/exp/env.clj
@@ -1,4 +1,5 @@
 (ns exp.env
+  (:use clojure.test)
   (:require [edu.berkeley.ai.util :as util])
   (:import [java.util Set Map])
   )
@@ -81,6 +82,26 @@
 
 (defn state-matches-map? [fs m]
   (every? (fn [[k v]] (= (get-var fs k) v)) m))
+
+(deftest test-logging-factored-state
+  (let [si {:a 1 :b 2 :c 3}
+        s1 (get-logger si #{:a})
+        s2 (set-var s1 :a 1)
+        s3 (set-var s1 :b 2)
+        s4 (set-var (set-var s1 :a 1) :b 2)
+        s5 (set-var (set-var s1 :a 1) :c 4)
+        ss [s1 s2 s3 s4 s5]
+        equal-sets [#{0 1} #{2 3} #{4}]
+        ]
+    (is (= (map as-map          ss) [si si si si (assoc si :c 4)]))
+    (is (= (map extract-effects ss) [{} {:a 1} {:b 2} {:a 1 :b 2} {:a 1 :c 4}]))
+    (is (= (map ooc-effects     ss) [{} {} {:b 2} {:b 2} {:c 4}]))
+    (doseq [es equal-sets] 
+      (is (apply = (map ss es)))
+      (is (apply = (map (comp hash ss) es))))
+    (doseq [[s1 s2] (util/combinations equal-sets 2), i1 s1, i2 s2]
+      (is (not (= i1 i2)))
+      (is (not (= (hash i1) (hash i2)))))))
 
 ;; TODO: make method so we can use transients?
 
