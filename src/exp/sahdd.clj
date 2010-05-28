3a8f626218e7a4849e4cdf47ee064a13bfb72c94
Jason Wolfe <jason@w01fe.com>
2010-05-27 20:01:05 -0700

    Refactored, really working (correct) sahucs-simple.

--- src/exp/sahdd.clj
+++ src/exp/sahdd.clj
@@ -9,24 +9,59 @@
 ; Note: hard (impossible?) to unify down and up.
 ; Best: at least have common interface, shared parts, to simplify.
 
+; Also fancier info going up, abstracted goals, etc.  
+
 ;; Also pass in antagonistic measure ? 
 
+;; Should fix cost-order of subproblems.
+ ;; I.e., right now, even if we have a given threshold, we return all states up to that.
+ ;; Should return just first state, since it represents new alternative at higher level.
+ ;; Ideally, this should just fall out of proper thinking.
+
+; Question: what is general way to do this? 
+; Note: to think of this as like SAHA, always refining a given outcome state.
+;    Can think: always refining *abstracted* outcome state.
+;    Note key difference: in SAHA we're doing bidi, in UCS we do forward dijkstra
+
+; Also need goal-hiding, etc. ?
+
+; Basic difference betwen top-down and bottom-up:
+ ; top-down: give me updates since X
+ ; bottom-up: give me updates as you get them, only of particular form. 
+ ; For now, fix this, then extend. 
+
+
+; We should still (unfortunately) never close open subproblems with cycles.  
+
+;; For now, how do we pass states up?
+  ; Store index / partial list in SubproblemEntry?
+  ; Store cost and loop through?
+  ; Keep queue, upwards pointers? 
+  ;  Ideally, have single queue, "fingers" into it in each subproblem. 
+  ;   Can almost do this with lazy seqs... ?!
+
+
+(defn viable? [reward cutoff]
+  (and (> reward Double/NEGATIVE_INFINITY)
+       (>= reward cutoff)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Queues ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn make-queue-priority [reward]
+  [(- reward)])
 
 (defn make-queue [initial-elements]
   (let [q (queues/make-graph-search-pq)]
-    (queues/g-pq-add! q :dummy Double/POSITIVE_INFINITY)
-    (queues/pq-add-all! q initial-elements)
+    (queues/g-pq-add! q :dummy (make-queue-priority Double/NEGATIVE_INFINITY))
+    (doseq [[e r] initial-elements] (queues/pq-add! q e (make-queue-priority r)))
     q))
 
 (defn empty-queue [queue]
   (queues/pq-remove-all! queue)
-  (queues/g-pq-replace! queue :dummy Double/POSITIVE_INFINITY))
+  (queues/g-pq-replace! queue :dummy (make-queue-priority Double/NEGATIVE_INFINITY)))
 
 (defn queue-best-reward [queue]
-  (- (nth (queues/g-pq-peek-min queue) 1)))
+  (- (first (nth (queues/g-pq-peek-min queue) 1))))
 
 (defn queue-best-and-next-reward [queue]
   (let [[best bc] (queues/pq-remove-min-with-cost! queue)
@@ -34,77 +69,96 @@
     (queues/pq-replace! queue best bc)
     [best next-reward]))
 
-(defn queue-cutoff? [queue reward-threshold]
+(defn queue-cutoff [queue min-reward]
   (let [cutoff (double (queue-best-reward queue))]
-    (or (= cutoff Double/NEGATIVE_INFINITY) (< cutoff reward-threshold))))
-
-(defn refine-open [child-queue goal-atom goal-fn expand-fn reward-fn single-goal? reward-threshold]
-  (when-not (queue-cutoff? child-queue reward-threshold)
-    (let [[best next-best-reward] (queue-best-and-next-reward child-queue)]
-      (if-let [g (goal-fn best)]
-            (do (if single-goal? 
-                    (empty-queue child-queue)
-                  (queues/pq-remove! child-queue best)) ;; TODO: Is this always right? 
-                (assert (not (contains? @goal-atom g)))
-                (swap! goal-atom assoc g (reward-fn best)))
-          (do (queues/pq-add-all! child-queue 
-                                  (for [x (expand-fn best (max next-best-reward reward-threshold (reward-fn best)))] ;; TODO
-                                    [x (- (reward-fn x))]))
-              (let [r (reward-fn best)] 
-                (if (= r Double/NEGATIVE_INFINITY) 
-                    (queues/pq-remove! child-queue best)
-                  (queues/pq-replace! child-queue best (- r))))))
-      (recur child-queue goal-atom goal-fn expand-fn reward-fn single-goal? reward-threshold))))
-
+    (when-not (viable? cutoff min-reward) cutoff)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Outcome maps ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn stitch-outcome-map [outcome-map state reward-to-state]
-  (util/map-map1 
-   (fn [[outcome-state local-reward]]
-     [(vary-meta (env/apply-effects state (env/extract-effects outcome-state)) assoc 
-                 :opt (concat (:opt (meta state)) (:opt (meta outcome-state))))
-      (+ reward-to-state local-reward)]) 
-   outcome-map))
+
+(defn generalize-outcome-pair [[outcome-state reward] gen-state reward-to-gen-state]
+  [(vary-meta (env/apply-effects gen-state (env/extract-effects outcome-state)) assoc 
+              :opt (concat (:opt (meta gen-state)) (:opt (meta outcome-state))))
+   (+ reward reward-to-gen-state)])
+
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Subproblems ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-
-
 (defprotocol Subproblem 
-  (sub-max-open-reward [sp])
-  (sub-refine          [sp min-reward max-reward]))
+  (sub-refine [sp min-reward] "Output is [result reward], reward >= min, or new threshold < min."))
 
-(declare se-goal se-max-reward se-refine)
+(declare se-goal se-refine)
 
 (defn pretty-state [s]
   (dissoc (env/as-map (or s {})) :const))
 
-(deftype OpenSubproblem [name child-queue result-atom single-goal?] :as this
+(deftype OpenSubproblem [name child-queue] :as this
   Subproblem
-  (sub-max-open-reward [] (queue-best-reward child-queue))
-  (sub-refine           [min-reward max-reward]
-    ;(println "\nRefining " (second  name) (first name) "in range [" min-reward max-reward "]" "\n" (util/map-keys pretty-state @result-atom)  "\n"(map #(vector (pretty-state (:initial-state (first %))) (map env/action-name (:remaining-actions (first %))) (next %)) (queues/pq-peek-pairs child-queue)))
-    (assert (<= min-reward max-reward))    
-;    (println "\nRefining " (second  name) (first name) "\n" (util/map-keys identity @result-atom)  "\n"(map #(cons (identity (first %)) (next %)) (queues/pq-peek-pairs child-queue)))    
-    (refine-open child-queue result-atom se-goal se-refine se-max-reward single-goal? min-reward)
-    ;(println "\nDONE Refining " (second  name) (first name) "\n" (util/map-keys pretty-state @result-atom)  "\n" (map #(vector (pretty-state (:initial-state (first %))) (map env/action-name (:remaining-actions (first %))) (next %)) (queues/pq-peek-pairs child-queue)))
-    (util/filter-map #(<= #_ min-reward (val %) max-reward) @result-atom)))
-  ; Note: if we use min-reward here, we have to keep track of which closed states we've used when computing max-open0-reward as well.  ?!
+  (sub-refine           [min-reward]
+    (util/print-debug 1 "\nRefining " (second  name) (first name) "with min-reward [" min-reward "]" "\n" (map #(vector (pretty-state (:initial-state (first %))) (map env/action-name (:remaining-actions (first %))) (next %)) (queues/pq-peek-pairs child-queue)))
+    (loop []                        
+;      (println (queue-best-reward child-queue))
+      (or (queue-cutoff child-queue min-reward) 
+          (let [[best next-best-reward] (queue-best-and-next-reward child-queue)]
+            (if-let [g (se-goal best)]
+              (do (queues/pq-remove! child-queue best) g)
+              (let [[[_ new-rew] & new-items] (se-refine best (max min-reward next-best-reward))]
+                (queues/pq-replace! child-queue best (make-queue-priority new-rew))
+                (doseq [[ni nr] new-items] (queues/pq-add! child-queue ni (make-queue-priority nr)))
+                (recur))))))))
 
 (declare make-subproblem-entry)
 
-(defn make-open-subproblem [name state action single-goal?]
+(defn make-subproblem-entries [action state]
   (if (env/primitive? action)
-    (OpenSubproblem name
-     (make-queue nil)
-     (atom (if-let [[ss r] (and (env/applicable? action state) (env/successor action state))] {ss r} {}))
-     single-goal?)
-    (OpenSubproblem name
-     (make-queue (for [ref (hierarchy/immediate-refinements action state)]
-                   [(make-subproblem-entry state 0.0 ref) 0.0]))
-     (atom {})
-     single-goal?)))
+      (when-let [p (and (env/applicable? action state) (env/successor action state))]
+        [(make-subproblem-entry p nil)])
+    (for [ref (hierarchy/immediate-refinements action state)]
+      (make-subproblem-entry [state 0] ref))))
+
+(defn make-open-subproblem [name state action]
+  (OpenSubproblem name (make-queue (make-subproblem-entries action state))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Shared Subproblems ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; An interface to allow subproblems to be shared, while pretending they have single consumer.
+;;; Not thread safe. 
+
+(deftype SharedSubproblemCache [subproblem result-vec-atom next-reward-atom]
+  Subproblem
+  (sub-refine [min-reward]
+    (util/print-debug 2 "SSPC" min-reward (Thread/sleep 10))               
+    (assert (viable? @next-reward-atom min-reward))
+    (let [result (sub-refine subproblem min-reward)]
+      (if (number? result)
+          (reset! next-reward-atom result)
+        (do (swap! result-vec-atom conj result)
+            (reset! next-reward-atom (nth result 1))))
+      result)))
+
+(defn make-shared-subproblem-cache [subproblem]
+  (SharedSubproblemCache subproblem (atom []) (atom Double/POSITIVE_INFINITY)))
+
+
+(deftype SharedSubproblem [cache index-atom]
+  Subproblem
+  (sub-refine [min-reward]
+   (let [{:keys [subproblem result-vec-atom next-reward-atom]} cache]
+     (util/print-debug 2 "SSP" min-reward @next-reward-atom (Thread/sleep 10)) 
+    (if (< @index-atom (count @result-vec-atom))
+          (let [[_ rew :as result] (nth @result-vec-atom @index-atom)]
+            (if (>= rew min-reward)
+                (do (swap! index-atom inc) result)
+              rew))
+        (if (viable? @next-reward-atom min-reward)
+            (do (sub-refine cache min-reward)
+                (recur min-reward))
+          @next-reward-atom)))))
+
+(defn make-shared-subproblem [cache]
+  (SharedSubproblem cache (atom 0)))
 
 
 (def #^HashMap *subproblem-cache*)
@@ -118,66 +172,48 @@
          (.put *subproblem-cache* name# x#)
          x#))))
 
-(defn create-open-subproblem [state action]
+(defn get-open-subproblem-instance [state action]
 ;  (println "\nGetting subproblem" (env/as-map state) (env/action-name action) "\n")
   (let [context   (env/precondition-context action state)
         name     [(env/extract-context state context) (env/action-name action)]]
-    (get-subproblem-instance name (make-open-subproblem name (env/get-logger state context) action false))))
-
-; SAS, SAAS, SASAS
-;(defmulti create-subproblem (fn [state action ...])) ?
+    (make-shared-subproblem 
+     (get-subproblem-instance name 
+       (make-shared-subproblem-cache
+        (make-open-subproblem name (env/get-logger state context) action))))))
 
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Subproblem Entries ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defprotocol SubproblemEntry
+(defprotocol PSubproblemEntry
   (se-goal       [se])
-  (se-max-reward [se])
-  (se-refine     [se new-threshold]))
+  (se-refine     [se new-threshold] "Returns list of new queue entries; first is always self."))
 
-(deftype SubproblemSolutionEntry [initial-state reward]
-  Object
-  (equals        [y] (= initial-state (:initial-state y)))
-  (hashCode      [] (hash initial-state))
-  SubproblemEntry
-  (se-goal       [] initial-state)
-  (se-max-reward [] reward)
-  (se-refine     [new-threshold] (throw (RuntimeException. "Not refinable"))))
-
-
-
-(deftype SubproblemPartialEntry [hash-code max-reward-atom initial-state reward child-subproblem remaining-actions] :as this
+(deftype SubproblemEntry [hash-code initial-state reward child-subproblem remaining-actions] :as this
   Object
   (equals [y] (or (identical? this y) 
                   (and (identical? child-subproblem (:child-subproblem y)) 
                        (= remaining-actions (:remaining-actions y)))))
   (hashCode [] hash-code)
-  SubproblemEntry
-  (se-goal       [] nil)
-  (se-max-reward [] @max-reward-atom)
-  (se-refine     [new-threshold]
-    (let [old-thresh @max-reward-atom
-          results  (sub-refine child-subproblem (- new-threshold reward) (- @max-reward-atom reward))
-          new-max-reward (+ (sub-max-open-reward child-subproblem) reward)]
-      (assert (< new-max-reward @max-reward-atom))
-      (reset! max-reward-atom new-max-reward)
-;      (println "got " results " new cutoff " old-thresh (se-max-reward this) reward new-threshold "stitched" (stitch-outcome-map results initial-state reward) )
-      (for [[s r] (stitch-outcome-map results initial-state reward)]
-        (make-subproblem-entry s r remaining-actions)))))
+  PSubproblemEntry
+  (se-goal       [] (when (nil? child-subproblem) [initial-state reward]))
+  (se-refine     [min-reward]
+    (util/print-debug 2 "SE-R" min-reward (Thread/sleep 10)) 
+    (assert child-subproblem)
+    (let [result (sub-refine child-subproblem (- min-reward reward))]
+      (if (number? result) 
+          [[this (+ result reward)]]
+        [[this (nth result 1)] 
+         (make-subproblem-entry (generalize-outcome-pair result initial-state reward) remaining-actions)]))))
 
-(defn make-subproblem-entry [initial-state reward remaining-actions]
-  #_(if (empty? remaining-actions) 
-      (println "DONE"  initial-state reward)
-    (println "PARTIAL" initial-state reward (map env/action-name remaining-actions)))
-  (if-let [[f & r] (seq remaining-actions)]
-    (let [sp (create-open-subproblem initial-state f)]
-      (SubproblemPartialEntry 
-       (unchecked-add (int (System/identityHashCode sp)) 
-                      (unchecked-multiply (int 13) (int (hash r))))
-       (atom reward) initial-state reward sp r))
-    (SubproblemSolutionEntry initial-state reward)))
-
+(defn make-subproblem-entry [[initial-state reward] remaining-actions]
+  (let [[f & r] remaining-actions
+        sp (when f (get-open-subproblem-instance initial-state f))]
+    [(SubproblemEntry 
+      (unchecked-add (int (if sp (System/identityHashCode sp) 0)) 
+                     (unchecked-multiply (int 13) (int (hash r))))
+      initial-state reward sp r)
+     reward]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Drivers  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -185,10 +221,11 @@
   (let [e     (hierarchy/env henv)
         init  (env/initial-logging-state e)]
     (binding [*subproblem-cache* (HashMap.)]
-      (when-first [p (sub-refine (make-open-subproblem [:init] init  
-                                   (hierarchy/TopLevelAction e [(hierarchy/initial-plan henv)]) true)
-                                 Double/NEGATIVE_INFINITY Double/POSITIVE_INFINITY)]
-        (second p)))))
+      (let [tla (hierarchy/TopLevelAction e [(hierarchy/initial-plan henv)])
+            top (make-open-subproblem [:init] init tla)
+            result (sub-refine top Double/NEGATIVE_INFINITY)]
+        (when (not (number? result))
+          (second result))))))
 
 ;; For SAHA, nothing is open.
 ;  Or strcture is same.
