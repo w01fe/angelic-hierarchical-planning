406abe20b0925001282c4d931091e69a15cd4a9a
Jason Wolfe <jason@w01fe.com>
2011-01-21 18:50:03 -0800

    Remove taxi from sas-prboelms

--- src/angelic/domains/sas_problems.clj
+++ src/angelic/domains/sas_problems.clj
@@ -32,7 +32,7 @@
 (defn sas-sample-files [n]
   (into {}
     (concat  
-      [["2x2-taxi"  (taxi/write-taxi-strips (taxi/make-random-taxi-env 2 2 2 1))]]
+      ;     [["2x2-taxi"  (taxi/write-taxi-strips (taxi/make-random-taxi-env 2 2 2 1))]]
       (for [domain ["elevators" "openstacks" "parcprinter" "pegsol" "scanalyzer" #_ "sokoban" "transport" "woodworking"]]
         [(str "IPC6-" domain)
          (str "/Users/jawolfe/Projects/research/IPC/ipc2008-no-cybersec/seq-opt/" domain "-strips/p"
