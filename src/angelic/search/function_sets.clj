149257d934925d4a03c0211705038b34576edc54
Jason Wolfe <jason@w01fe.com>
2011-05-24 11:20:08 -0700

    Wrap

--- src/angelic/search/function_sets.clj
+++ src/angelic/search/function_sets.clj
@@ -19,13 +19,15 @@
 
 (defprotocol FunctionSet
   (fs-name    [fs]           "Arbitrary name to identify fs")
-  (primitive? [fs] "needed for SAHTN, etc.")
+  (primitive? [fs]           "needed for SAHTN, etc.")
   (precondition-context-set [fs input-set] "Relevant set of variables")  
-  (apply-opt  [fs input-set] "[output-set upper-reward-bound status]. output nil if empty or -inf. Status is :live/:blocked/:solved.")
-  (apply-pess [fs input-set] "[output-set upper-reward-bound status]. output nil if empty or -inf. Status is :live/:blocked/:solved.")  
+  (apply-opt  [fs input-set] "[output-set upper-reward-bound status].
+       output nil if empty or -inf. Status is :live/:blocked/:solved.")
+  (apply-pess [fs input-set] "[output-set upper-reward-bound status].
+       output nil if empty or -inf. Status is :live/:blocked/:solved.")  
   (child-seqs [fs input-set] "seq of seqs of FunctionSets. Only valid if above is :live.
-                              (= :live (status fs s)) ==>
-                                 (subset? (child-seqs fs s-subset) (child-seqs fs s)) (for names)"))
+       (= :live (status fs s)) ==>
+        (subset? (child-seqs fs s-subset) (child-seqs fs s)) (for names)"))
 
 (defn extract-context [fs input-set]
   (state/extract-context input-set (precondition-context-set fs input-set)))
@@ -50,9 +52,12 @@
     FunctionSet
     (fs-name    [fs]           n)
     (primitive? [fs] prim?)
-    (precondition-context-set [fs input-set] (angelic/precondition-context-set action input-set))
-    (apply-opt  [fs input-set] (output-or-nil angelic/optimistic-set-and-reward status-fn action input-set))
-    (apply-pess [fs input-set] (output-or-nil angelic/pessimistic-set-and-reward status-fn action input-set))        
+    (precondition-context-set [fs input-set]
+      (angelic/precondition-context-set action input-set))
+    (apply-opt  [fs input-set]
+      (output-or-nil angelic/optimistic-set-and-reward status-fn action input-set))
+    (apply-pess [fs input-set]
+      (output-or-nil angelic/pessimistic-set-and-reward status-fn action input-set))        
     (child-seqs [fs input-set] (child-seq-fn input-set)))))
 
 (defmethod print-method angelic.search.function_sets.FunctionSet [s o]
@@ -68,7 +73,7 @@
     (if (or (empty? ref) (seq constraint))
       (cons
        (env-util/make-factored-primitive [:noop constraint]
-                                         (util/map-vals util/safe-singleton constraint) {} 0)     
+         (util/map-vals util/safe-singleton constraint) {} 0)     
        ref)
       ref)))
 
@@ -102,7 +107,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Utils ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn =-state-sets [s1 s2]
-  (util/assert-is (clojure.set/subset? (state/current-context s1) (state/current-context s2)) "%s" [s1 s2])
+  (util/assert-is (clojure.set/subset? (state/current-context s1)
+                                       (state/current-context s2)) "%s" [s1 s2])
   (= s1 s2))
 
 (defn transfer-effects [to-set from-set]
